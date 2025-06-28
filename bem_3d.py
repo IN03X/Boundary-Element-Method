6e4a804544bcc1b958d566116eac61f4460f571d
Runbang Wang <1048675056@qq.com>
2025-06-28 17:32:47 +0800

    验证了一下长方体的BEM，频率需要比球体小很多才是正常的，应该跟边界处的突变有关，也可能是我长方体的mesh划分太大了

--- bem_3d.py
+++ bem_3d.py
@@ -587,7 +587,8 @@ class Mesh2Field:
 if __name__ == "__main__":
     #下面这一大段是在生成边界条件,这是由于stl不包含材质信息,在NN训练时最好考虑使用包含边界条件的数据
     mesh = SurfaceMesh()
-    mesh_file = "sphere_radius_1.0_resolution_15.stl"
+    #mesh_file = "sphere_radius_1.0_resolution_15.stl"
+    mesh_file = "cuboid.stl"
     mesh.load_from_stl(mesh_file)
     bc_types = np.zeros(mesh.N)
     bc_values = np.zeros(mesh.N, dtype=np.complex128)
@@ -604,8 +605,9 @@ if __name__ == "__main__":
     #基本输入为: stl模型, 边界条件, 频率(注意一次只能模拟单频率)
     #生成声场自定义参数输入为: 声场所在平面, 声场范围, 声场网格分辨率
     #输出为：声场声压， 声场声势， 声场各点位置
-    mesh_file = "sphere_radius_1.0_resolution_15.stl"
-    mesh2field_test = Mesh2Field(frequency=60,mesh_file=mesh_file,bc_types=bc_types,bc_values=bc_values)
+    #mesh_file = "sphere_radius_1.0_resolution_15.stl"
+    mesh_file = "cuboid.stl"
+    mesh2field_test = Mesh2Field(frequency=10,mesh_file=mesh_file,bc_types=bc_types,bc_values=bc_values)
     #计算边界上的声势和振速
     mesh2field_test.calc_bc_phiv()
     #计算特定点的声势
